73f1e76b3ac253a9c7cf52fb578e7723bced7797
t-zochi <mojamoja.000@gmail.com>
2019-07-03 16:20:51 +0900

    bugfix

--- blog/views.py
+++ blog/views.py
@@ -21,13 +21,13 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.auther = request.user
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm()
-    return render(request, "blog//post_edit.html", {"form": form})
+    return render(request, "blog/post_edit.html", {"form": form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
